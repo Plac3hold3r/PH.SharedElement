0cc825bd97357ae823999c26f3dd741e3fae876b
Plac3hold3r <Plac3hold3r+github@outlook.com>
2017-09-18 20:09:01 +0200

    Fix java activity class names

--- src/Placeholder.SharedElement.Droid/DroidConstants.cs
+++ src/Placeholder.SharedElement.Droid/DroidConstants.cs
@@ -2,7 +2,7 @@
 {
     public static class DroidConstants
     {
-        public const string SharedElement_Views_Namespace = "Placeholder.SharedElement.droid.views.";
+        public const string SharedElement_Views_Namespace = "placeholder.sharedelement.droid.views.";
         public const string Transform_Animate_Image_Tag = "anim_image";
         public const string Transform_Animate_Text_Tag = "anim_text";
     }
