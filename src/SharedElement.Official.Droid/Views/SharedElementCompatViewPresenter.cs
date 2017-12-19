8d997e0a7f10806b910f3d54ca9a3d6efb742401
Plac3hold3r <Plac3hold3r+github@outlook.com>
2017-12-19 18:32:42 +0200

    Add support for fragment transitions

--- src/SharedElement.Official.Droid/Views/SharedElementCompatViewPresenter.cs
+++ src/SharedElement.Official.Droid/Views/SharedElementCompatViewPresenter.cs
@@ -1,11 +1,14 @@
 ﻿using Android.App;
 using Android.Content;
+using Android.OS;
 using Android.Support.V4.App;
 using Android.Support.V4.Util;
 using Android.Support.V4.View;
 using Android.Views;
 using MvvmCross.Core.ViewModels;
+using MvvmCross.Droid.Support.V4;
 using MvvmCross.Droid.Support.V7.AppCompat;
+using MvvmCross.Droid.Views;
 using MvvmCross.Droid.Views.Attributes;
 using MvvmCross.Platform;
 using MvvmCross.Platform.Logging;
@@ -91,6 +94,102 @@ namespace SharedElement.Official.Droid.Views
             }
 
             return (elements, transitionElementPairs.ToArray());
+        }
+
+        protected override void PerformShowFragmentTransaction(
+            Android.Support.V4.App.FragmentManager fragmentManager,
+            MvxFragmentPresentationAttribute attribute,
+            MvxViewModelRequest request)
+        {
+            var fragmentName = FragmentJavaName(attribute.ViewType);
+
+            IMvxFragmentView fragment = null;
+            if (attribute.IsCacheableFragment)
+            {
+                fragment = (IMvxFragmentView)fragmentManager.FindFragmentByTag(fragmentName);
+            }
+            fragment = fragment ?? CreateFragment(attribute, fragmentName);
+
+            var fragmentView = fragment.ToFragment();
+
+            // MvxNavigationService provides an already instantiated ViewModel here
+            if (request is MvxViewModelInstanceRequest instanceRequest)
+            {
+                fragment.ViewModel = instanceRequest.ViewModelInstance;
+            }
+
+            // save MvxViewModelRequest in the Fragment's Arguments
+            var bundle = new Bundle();
+            var serializedRequest = NavigationSerializer.Serializer.SerializeObject(request);
+            bundle.PutString(ViewModelRequestBundleKey, serializedRequest);
+
+            if (fragmentView != null)
+            {
+                if (fragmentView.Arguments == null)
+                {
+                    fragmentView.Arguments = bundle;
+                }
+                else
+                {
+                    fragmentView.Arguments.Clear();
+                    fragmentView.Arguments.PutAll(bundle);
+                }
+            }
+
+            var ft = fragmentManager.BeginTransaction();
+
+            OnBeforeFragmentChanging(ft, fragmentView, attribute, request);
+
+            if (attribute.AddToBackStack == true)
+                ft.AddToBackStack(fragmentName);
+
+            OnFragmentChanging(ft, fragmentView, attribute);
+
+            ft.Replace(attribute.FragmentContentId, (Android.Support.V4.App.Fragment)fragment, fragmentName);
+            ft.CommitAllowingStateLoss();
+
+            OnFragmentChanged(ft, fragmentView, attribute);
+        }
+
+        protected virtual void OnBeforeFragmentChanging(
+            Android.Support.V4.App.FragmentTransaction ft,
+            Android.Support.V4.App.Fragment fragmentView,
+            MvxFragmentPresentationAttribute attribute,
+            MvxViewModelRequest request)
+        {
+            var controlTags = string.Empty;
+            request.PresentationValues?.TryGetValue(SharedConstants.Animate_Tag, out controlTags);
+
+            if (!string.IsNullOrEmpty(controlTags))
+            {
+                var elements = new List<string>();
+
+                foreach (var controlTag in controlTags.Split('|'))
+                {
+                    View control = CurrentActivity.FindViewById(Android.Resource.Id.Content).FindViewWithTag(controlTag);
+
+                    if (control == null)
+                    {
+                        Log.Warn("No control with a tag \"{0}\" was found on the current view.", controlTag);
+                        continue;
+                    }
+                    control.Tag = null;
+
+                    var transitionName = ViewCompat.GetTransitionName(control);
+                    if (string.IsNullOrEmpty(transitionName))
+                    {
+                        Log.Warn("A XML {0} is required in order to transition a control when navigating.", nameof(transitionName));
+                        continue;
+                    }
+
+                    ft.AddSharedElement(control, transitionName);
+                    elements.Add($"{controlTag}:{transitionName}");
+                }
+
+                fragmentView.Arguments.PutString(DroidConstants.Transition_Name_Key, string.Join("|", elements));
+            }
+
+            OnBeforeFragmentChanging(ft, fragmentView, attribute);
         }
     }
 }
